ca95059ba149525c0bf6b3b8047116de3420881d
Daniel Uher <duher18@univ.yo.oulu.fi>
2020-08-23 21:21:56 +0300

    lkjdfg

--- install_nbgrader.bat
+++ install_nbgrader.bat
@@ -1,11 +1,7 @@
 @echo off
-call conda activate
-call conda create --name nbgrader_env --clone base
 call conda activate nbgrader_env
 call conda install gitpython
 call pip install tabulate
-call pip install opencv-python
-call pip install opencv-contrib-python
 call conda install jupyter
 call conda install -c conda-forge nbgrader
 call jupyter nbextension install --sys-prefix --py nbgrader --overwrite
