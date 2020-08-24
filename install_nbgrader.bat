67850a6a2125a18c9154cf1995b571b20859eb0b
Daniel Uher <duher18@univ.yo.oulu.fi>
2020-08-24 08:08:42 +0300

    repo name update

--- install_nbgrader.bat
+++ install_nbgrader.bat
@@ -8,7 +8,7 @@ call conda install -c conda-forge nbgrader
 call jupyter nbextension install --sys-prefix --py nbgrader --overwrite
 call jupyter nbextension enable --sys-prefix --py nbgrader
 call jupyter serverextension enable --sys-prefix --py nbgrader
-git clone https://github.com/daniuher/nbgrader_custom.git nbgrader
+git clone https://github.com/daniuher/nbgrader_toolbox.git nbgrader
 call cd nbgrader
 call git checkout installer
 pause
