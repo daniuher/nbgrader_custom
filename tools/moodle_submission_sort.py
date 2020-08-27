4be51d0f45cdc29ed1f4aa3aec4641cfbfaa60d7
daniuher <daniel-uher@hotmail.com>
2020-08-27 15:47:53 +0300

    moodle sort updated for multi-student submissions

--- tools/moodle_submission_sort.py
+++ tools/moodle_submission_sort.py
@@ -18,12 +18,25 @@ from distutils.dir_util import copy_tree
 
 
 def getStudentNumberFromNotebook(ntbpath):
+    # dig out the student number(s) from the notebook
+    
     nb = nbformat.read(ntbpath, as_version = 4)
     stuff = nb.cells[0].source
     for line in stuff.splitlines():
         if 'Student ID:' in line:
-            student_number = re.sub("[^0-9]", "", line)
+            temp_number = re.sub("[^0-9]", "", line)
+            student_number = re.findall('.......', temp_number)
             return student_number
+
+
+
+def deleteCheckpoints(submissionPath):
+    # delete .ipynb_checkpoints
+    
+    if os.path.isdir(submissionPath+'\\.ipynb_checkpoints'):
+        shutil.rmtree(submissionPath+'\\.ipynb_checkpoints')
+    
+        
 
 
 # filename = input('file: ')
@@ -50,7 +63,8 @@ files = [ x for x in files if ".ipynb" not in x ]
 timestamps = list()
 with zipfile.ZipFile(fileitself, 'r') as zip_ref:
     for sub in zip_ref.infolist():
-        timestamps.append(sub.date_time)
+        if sub.filename.split('/')[-1] == correct_name[0]:
+            timestamps.append(sub.date_time)
     zip_ref.extractall(filepath+'\\extracted')
 # os.remove(filepath)
 extractedpath = filepath+'\\extracted'
@@ -61,41 +75,48 @@ submissions = os.listdir(extractedpath)
 for submission in submissions:
     
     temp_path = extractedpath+'\\'+submission # path to the extracted submission weird-name folder
+    deleteCheckpoints(temp_path)
     temp_file_ipynb = os.listdir(temp_path)[0] # submitted_file.ipynb
     temp_file = temp_file_ipynb.split('.')[0] # submitted_file 
     assignment_number = temp_file.split('_')[1] 
     try:
-        student_number = temp_file.split('_')[2]
+        student_numbers = temp_file.split('_')[2]
     except:
         # no student number provided
-        student_number = getStudentNumberFromNotebook(temp_path+'\\'+temp_file_ipynb)
-                
-    if not student_number:
-        misses += 1
-        student_number = 'no_student_ID_'+str(misses)
-    
-    if not os.path.isdir(filepath+'\\'+student_number):
-        os.mkdir(filepath+'\\'+student_number)
-    
-    student_path = filepath+'\\'+student_number
-    if not os.path.isdir(student_path+'\\'+assignment_name):
-        os.mkdir(student_path+'\\'+assignment_name)
+        student_numbers = getStudentNumberFromNotebook(temp_path+'\\'+temp_file_ipynb)
+                    
         
-    # timestamp   
-    
-    # timestamp = os.path.getmtime(temp_path+'\\'+temp_file_ipynb)
-    # timestamp = datetime.fromtimestamp(timestamp)
     timestamp = timestamps.pop(0)
     timestamp = datetime(*timestamp[0:])
-    text_file = open(student_path+'\\'+assignment_name+'\\'+"timestamp.txt", "w")
-    text_file.write(str(timestamp))
-    text_file.close()
     
-    
-    # Copying the supporting files
-    copy_tree(released_assignment_path, student_path+'\\'+assignment_name)
-    os.rename(temp_path+'\\'+temp_file_ipynb, temp_path+'\\'+correct_name[0])
-    shutil.move(temp_path+'\\'+correct_name[0], student_path+'\\'+assignment_name+'\\'+correct_name[0])
+    # create folder for each student seperetaly
+    for student_number in student_numbers:
+        
+        if not student_number:
+            misses += 1
+            student_number = 'no_student_ID_'+str(misses)
+        
+        if not os.path.isdir(filepath+'\\'+student_number):
+            os.mkdir(filepath+'\\'+student_number)
+        
+        student_path = filepath+'\\'+student_number
+        if not os.path.isdir(student_path+'\\'+assignment_name):
+            os.mkdir(student_path+'\\'+assignment_name)
+            
+        # timestamp   
+        
+        # timestamp = os.path.getmtime(temp_path+'\\'+temp_file_ipynb)
+        # timestamp = datetime.fromtimestamp(timestamp)
+        
+        text_file = open(student_path+'\\'+assignment_name+'\\'+"timestamp.txt", "w")
+        text_file.write(str(timestamp))
+        text_file.close()
+        
+        
+        # Copying the supporting files
+        copy_tree(released_assignment_path, student_path+'\\'+assignment_name)
+        os.rename(temp_path+'\\'+temp_file_ipynb, temp_path+'\\'+correct_name[0])
+        shutil.copy(temp_path+'\\'+correct_name[0], student_path+'\\'+assignment_name+'\\'+correct_name[0])
     
     
     # with zipfile.ZipFile(temp_path+'\\'+temp_file_ipynb, 'r') as zip_ref:
