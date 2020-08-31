52b297232caa67292af13a11264e1b78ddac5cba
daniuher <daniel-uher@hotmail.com>
2020-08-31 11:46:12 +0300

    moodle submission sort fix

--- tools/moodle_submission_sort.py
+++ tools/moodle_submission_sort.py
@@ -38,9 +38,18 @@ def deleteCheckpoints(submissionPath):
     
         
 
+def getSubmittedNotebookName(extractedSubmissionFolder):
+    # get the submitted notebook name
+    
+    files = os.listdir(extractedSubmissionFolder)
+    ntb_name = [ x for x in files if ".ipynb" in x ]
+    return ntb_name[0]
+
+
 
 # filename = input('file: ')
 filename = input('Name of the zip file: ')
+# filename = '521466S1-A1-Imaging-177674.zip'
 misses = 0
 
 filepath = os.getcwd()+'\\submitted'
@@ -55,7 +64,7 @@ assignment_name = filename.split('-')[1]+'-'+filename.split('-')[2]
 released_path = os.getcwd()+'\\release'
 released_assignment_path = released_path+'\\'+assignment_name
 files = os.listdir(released_assignment_path)
-correct_name = [ x for x in files if ".ipynb" in x ]
+correct_name = [ x for x in files if ".ipynb" in x ][0]
 files = [ x for x in files if ".ipynb" not in x ]
 
 
@@ -63,8 +72,7 @@ files = [ x for x in files if ".ipynb" not in x ]
 timestamps = list()
 with zipfile.ZipFile(fileitself, 'r') as zip_ref:
     for sub in zip_ref.infolist():
-        if sub.filename.split('/')[-1] == correct_name[0]:
-            timestamps.append(sub.date_time)
+        timestamps.append(sub.date_time)
     zip_ref.extractall(filepath+'\\extracted')
 # os.remove(filepath)
 extractedpath = filepath+'\\extracted'
@@ -75,15 +83,25 @@ submissions = os.listdir(extractedpath)
 for submission in submissions:
     
     temp_path = extractedpath+'\\'+submission # path to the extracted submission weird-name folder
-    deleteCheckpoints(temp_path)
-    temp_file_ipynb = os.listdir(temp_path)[0] # submitted_file.ipynb
-    temp_file = temp_file_ipynb.split('.')[0] # submitted_file 
+    temp_zip_file = os.listdir(temp_path)[0] # submitted_file.zip
+    temp_file = temp_zip_file.split('.')[0] # submitted_file 
     assignment_number = temp_file.split('_')[1] 
-    try:
-        student_numbers = temp_file.split('_')[2]
-    except:
-        # no student number provided
-        student_numbers = getStudentNumberFromNotebook(temp_path+'\\'+temp_file_ipynb)
+    
+    with zipfile.ZipFile(temp_path+'\\'+temp_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(temp_path+'\\sub')
+    os.remove(temp_path+'\\'+temp_zip_file)
+    deleteCheckpoints(temp_path+'\\sub')
+    ntb_name = getSubmittedNotebookName(temp_path+'\\sub')
+    shutil.move(temp_path+'\\sub\\'+ntb_name, temp_path+'\\'+correct_name)
+    shutil.rmtree(temp_path+'\\sub')
+    
+    student_numbers = getStudentNumberFromNotebook(temp_path+'\\'+correct_name)
+    
+    # try:
+    #     student_numbers = temp_file.split('_')[2]
+    # except:
+    #     # no student number provided
+    #     student_numbers = getStudentNumberFromNotebook(temp_path+'\\'+ntb_name)
                     
         
     timestamp = timestamps.pop(0)
@@ -105,7 +123,7 @@ for submission in submissions:
             
         # timestamp   
         
-        # timestamp = os.path.getmtime(temp_path+'\\'+temp_file_ipynb)
+        # timestamp = os.path.getmtime(temp_path+'\\'+temp_zip_file)
         # timestamp = datetime.fromtimestamp(timestamp)
         
         text_file = open(student_path+'\\'+assignment_name+'\\'+"timestamp.txt", "w")
@@ -115,16 +133,16 @@ for submission in submissions:
         
         # Copying the supporting files
         copy_tree(released_assignment_path, student_path+'\\'+assignment_name)
-        os.rename(temp_path+'\\'+temp_file_ipynb, temp_path+'\\'+correct_name[0])
-        shutil.copy(temp_path+'\\'+correct_name[0], student_path+'\\'+assignment_name+'\\'+correct_name[0])
+        os.remove(student_path+'\\'+assignment_name+'\\'+correct_name)
+        shutil.copy(temp_path+'\\'+correct_name, student_path+'\\'+assignment_name+'\\'+correct_name)
     
     
-    # with zipfile.ZipFile(temp_path+'\\'+temp_file_ipynb, 'r') as zip_ref:
+    # with zipfile.ZipFile(temp_path+'\\'+temp_zip_file, 'r') as zip_ref:
     #     zip_ref.extractall(student_path+'\\'+assignment_name)
     
     
     
-    # os.remove(temp_path+'\\'+temp_file_ipynb)
+    # os.remove(temp_path+'\\'+temp_zip_file)
     shutil.rmtree(temp_path)
     
 os.rmdir(extractedpath)    
